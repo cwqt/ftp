3e69d2e25b27d8e6708937c3fa1c541e11ae7f00
cass <dev@cass.si>
2021-01-16 12:20:52 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ node compile.js #add in stuff to skeleton
 FILECOUNT=$(find ./f -not -path '*/\.*' -type f | wc -l | awk '{ printf("%'"'"'d\n",$1); }')
 sed -i -e "s/FILE_COUNT/$FILECOUNT/g" ./_site/index.html
 
-COMMIT=$(curl --silent --header "PRIVATE-TOKEN: $GITLAB_API_KEY" "https://gitlab.com/api/v4/projects/23430748/repository/commits/ftp" | jq '.short_id' | tr -d \")
+COMMIT=$(curl --silent --header "PRIVATE-TOKEN: $GITLAB_API_KEY" "https://gitlab.com/api/v4/projects/23430748/repository/commits/master" | jq '.short_id' | tr -d \")
 sed -i -e "s/GIT_SHA/$COMMIT/g" ./_site/index.html
 
 cp -r www _site/
